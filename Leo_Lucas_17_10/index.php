46fb137e0ce8e504e8a64ce3d5b35f6e4f34d7e4
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-10-24 08:35:16 -0300

    Update index.php

--- Leo_Lucas_17_10/index.php
+++ Leo_Lucas_17_10/index.php
@@ -2,64 +2,76 @@
 <html lang="pt-br">
 
 <head>
-	<title>Projeto - Léo e Rogerio</title>
+	<title>Projeto</title>
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width,initial-scale=1"/> 
 	<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
 	<link rel="stylesheet icon" href=""/>
-    <!-- CSS only -->
+  <?php include 'conexao.php';?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
     
 <body>
+<form name="maikon" method="get" action="">
 <div class="row">
-<div class="col">
+<div class='col'>
 <label class="form-label">Cargo</label>
-<select class="form-select" aria-label="Default select example">
-  <option selected placeholder="0"</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
+<select class="form-select" name="sltCargo">
+  <option selected value="">Selecione</option>
+  <?php
+    $consulta = $cn->query("select cargo from tbl_empregos group by cargo");
+    while($exibe = $consulta->fetch(PDO::FETCH_ASSOC)){
+      echo "<option value='".$exibe['cargo']."'>".$exibe['cargo']."</option>";
+    } 
+  ?>
 </select>
 </div>
-<div class="col">
+<div class='col'>
 <label for="exampleInputEmail1" class="form-label">Area</label>
-<select class="form-select" aria-label="Default select example">
-<option selected placeholder="0"</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
+<select class="form-select" name="sltArea">
+  <option selected value="">Selecione</option>
+  <?php
+    $consulta = $cn->query("select area from tbl_empregos group by area");
+    while($exibe = $consulta->fetch(PDO::FETCH_ASSOC)){
+      echo "<option value='".$exibe['area']."'>".$exibe['area']."</option>";
+    } 
+  ?>
 </select>   
 </div>
 <br><br>
 <br><br>
 <button type="submit" class="btn btn-primary">Buscar</button>
 </div>
+</form>
 <br>
 <div class="row">
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Registro</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Cargo</th>
-      <th scope="col">Área</th>
-      <th scope="col">Salário</th>
-      <th scope="col">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
 
-    </tr>
-    <tr>
- 
-    </tr>
-    <tr>
+  <?php 
+  $cargo=$_GET['sltCargo'];
+  $area=$_GET['sltArea'];
+  if($cargo === "" || $area === ""){
+    
+  }
+  else{
+    $consulta = $cn->query("select * from tbl_empregos where Cargo = '$cargo' and Area = '$area'");
+    $campos = ['Registro','Nome','Cargo','Area','Salario','Status'];
+    echo "<table class='table'> <thead> <tr>";
+    foreach($campos as &$nome)
+      echo"<th scope='col'>$nome</th>";
+    echo "<th scope='col'>Alterar</th> <th scope='col'>Excluir</th> </tr> </thead>";
+    while($exibe = $consulta->fetch(PDO::FETCH_ASSOC)){
+      echo "<tr>";
+      foreach($campos as &$nome)
+        echo "<th>".$exibe[$nome]."</th>";
+      echo "<th><img style='width:35px; height:auto;' src='https://www.d-velop.de/wp-content/uploads/sites/10/2020/09/icon-digital-signieren-lila.png'></th>";
+      echo "<th><img style='width:35px; height:auto;' src='https://images.vexels.com/media/users/3/153297/isolated/preview/b394f1c0280879beb70bc51813fe1f41---cone-de-tra--o-colorido-de-lixeira-by-vexels.png'></th>";
+      echo "</tr>";
+    }
+    echo "</table>";
+  }
+  ?>
+  
 
-    </tr>
-  </tbody>
-</table>
 </div>
 
 </body>
