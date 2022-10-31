89878108e2825828b0e47dcf38179d17511ccf84
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-10-31 12:14:08 -0300

    Levemente melhorado

--- Leo_Lucas_17_10/index.php
+++ Leo_Lucas_17_10/index.php
@@ -12,11 +12,11 @@
 </head>
     
 <body>
-<form name="maikon" method="get" action="">
+<form name="frmpesquisa" method="get" action="">
 <div class="row">
 <div class='col'>
 <label class="form-label">Cargo</label>
-<select class="form-select" name="sltCargo">
+<select class="form-select" name="sltcargo">
   <option selected value="">Selecione</option>
   <?php
     $consulta = $cn->query("select cargo from tbl_empregos group by cargo");
@@ -28,7 +28,7 @@
 </div>
 <div class='col'>
 <label for="exampleInputEmail1" class="form-label">Area</label>
-<select class="form-select" name="sltArea">
+<select class="form-select" name="sltarea">
   <option selected value="">Selecione</option>
   <?php
     $consulta = $cn->query("select area from tbl_empregos group by area");
@@ -47,22 +47,26 @@
 <div class="row">
 
   <?php 
-  $cargo=$_GET['sltCargo'];
-  $area=$_GET['sltArea'];
-  if($cargo === "" || $area === ""){
-    
-  }
-  else{
+  $verificar = fn($campo) => isset($_GET[$campo]) && $_GET[$campo] != null;
+
+  if($verificar('sltcargo') && $verificar('sltarea')){
+
+    $cargo = $_GET['sltcargo'];
+    $area = $_GET['sltarea'];
     $consulta = $cn->query("select * from tbl_empregos where Cargo = '$cargo' and Area = '$area'");
     $campos = ['Registro','Nome','Cargo','Area','Salario','Status'];
+
     echo "<table class='table'> <thead> <tr>";
+
     foreach($campos as &$nome)
       echo"<th scope='col'>$nome</th>";
+
     echo "<th scope='col'>Alterar</th> <th scope='col'>Excluir</th> </tr> </thead>";
     while($exibe = $consulta->fetch(PDO::FETCH_ASSOC)){
       echo "<tr>";
       foreach($campos as &$nome)
         echo "<th>".$exibe[$nome]."</th>";
+        
       echo "<th><img style='width:35px; height:auto;' src='https://www.d-velop.de/wp-content/uploads/sites/10/2020/09/icon-digital-signieren-lila.png'></th>";
       echo "<th><img style='width:35px; height:auto;' src='https://images.vexels.com/media/users/3/153297/isolated/preview/b394f1c0280879beb70bc51813fe1f41---cone-de-tra--o-colorido-de-lixeira-by-vexels.png'></th>";
       echo "</tr>";
@@ -70,15 +74,9 @@
     echo "</table>";
   }
   ?>
-  
-
 </div>
-
 </body>
 </html>
 <style>
-
-body{
-  flex-direction:column;
-}
+body{flex-direction:column;}
 </style>
